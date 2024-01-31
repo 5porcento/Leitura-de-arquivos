4ee6c86b5bd786989817ee95f0468d39d98f681c
Kauan Pacheco <kauanpacheco.dev@gmail.com>
2024-01-30 21:47:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1;
+
+
+LeituraArquivo arquivo = new LeituraArquivo();
+
+var (sucesso, Linhas, QntLinhas) = arquivo.LerArquivo("C:\\TA_BLZ\\Aula_2\\ConsoleApp1\\Arquivos\\TextFile1.txt");
+if (sucesso)
+{
+    Console.WriteLine("o Numero de linhas no arquivo e: "+ QntLinhas);
+    foreach (string linha in Linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}else
+{
+    Console.WriteLine("Erro na leitura do arquivo");
+}
